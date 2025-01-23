705021aba43bf73d07324f791cbb2f7974b4d979
Halvardgithub <halvar3005@outlook.com>
2025-01-23 13:57:53 +0100

    merged weekly and monthly into one repo
    Now it runs locally in both cases, still needs improvements to the actual model fitting

--- isolated_run.R
+++ isolated_run.R
@@ -2,8 +2,13 @@
 source("train.R")
 source("predict.R")
 
+#test for weekly data, fails with nlag = 12 since the dataset is to small
 train_chap("example_data/trainData.csv", "example_data/model")
 predict_chap("example_data/model", "example_data/training_data.csv", "example_data/future_data.csv", "example_data/predictions.csv")
+
+#test for monthly data
+train_chap("example_data_monthly/trainData.csv", "example_data_monthly/model")
+predict_chap("example_data_monthly/model", "example_data_monthly/historic_data.csv", "example_data_monthly/future_data.csv", "example_data_monthly/predictions.csv")
 
 
 # library(tsibble)
