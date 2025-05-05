d3b1402a70c8c59cd6d23d4efd8edc615445d7ff
Halvardgithub <halvar3005@outlook.com>
2025-05-05 08:45:40 +0200

    Comparible with the first EWARS model

--- predict.R
+++ predict.R
@@ -15,6 +15,10 @@ library(dlnm)
 library(dplyr)
 source("lib.R")
 
+#for spatial effects
+library(sf)
+library(spdep)
+
 predict_chap <- function(model_fn, hist_fn, future_fn, preds_fn){
   #load(file = model_fn) #would normally load a model here
   
@@ -35,10 +39,7 @@ predict_chap <- function(model_fn, hist_fn, future_fn, preds_fn){
     df <- offset_years_and_months(df)
   }
   
-  #df$ID_year <- df$ID_year - min(df$ID_year) + 1 #makes the years 1, 2, ...
-  
-  df <-group_by(df, location) |>
-    mutate(ID_time = row_number()) #this is for a RW1 in time
+  df$ID_year <- df$ID_year - min(df$ID_year) + 1 #makes the years 1, 2, ...
   
   basis_meantemperature <- crossbasis(df$meantemperature, lag=nlag, 
                                       argvar = list(fun = "ns", knots = equalknots(df$meantemperature, 2)), 
@@ -50,13 +51,29 @@ predict_chap <- function(model_fn, hist_fn, future_fn, preds_fn){
                               arglag = list(fun = "ns", knots = nlag/2), group = df$ID_spat)
   colnames(basis_rainsum) = paste0("basis_rainsum.", colnames(basis_rainsum))
   
-  #basis_formula <- Cases ~ 1 + f(ID_spat, model='iid', replicate=ID_year) + f(week, model='rw1', cyclic=T, scale.model=T)
-  #lagged_formula <- Cases ~ 1 + f(ID_spat, model='iid', replicate=ID_year) + 
-   # f(week, model='rw1', cyclic=T, scale.model=T) + basis_meantemperature + basis_rainsum
+  lagged_formula <- Cases ~ 1 + f(ID_spat, model='iid', replicate=ID_year) + 
+    f(ID_time_cyclic, model='rw1', cyclic=T, scale.model=T) + 
+    basis_meantemperature + basis_rainsum
   
-  lagged_formula <- Cases ~ 1 + f(ID_spat, model='iid') + 
-    f(ID_time, model = "rw1", scale.model = T) +
-    f(ID_time_cyclic, model='rw1', cyclic=T, scale.model=T) + basis_meantemperature + basis_rainsum
+  # if(graph_fn != ""){
+  #   df$ID_spat_num2 <- df$ID_spat_num
+  #   
+  #   geojson <- st_read(graph_fn)
+  #   geojson <- st_make_valid(geojson) #some geojson files will giver errors later if this is not cleaned here
+  #   
+  #   df_locs <- unique(df$location)
+  #   geojson_red <- geojson[geojson$VARNAME_1 %in% df_locs, ] #only keeps the regions present in the dataframe
+  #   #both df_locs and geosjon_red should be in alfabetical order, believe the files are alfabetical in CHAP, might need to sort them
+  #   #if they are both alphabetical, and take care about which field in the geeojson, could be vietnamese or english (handle in CHAP?)
+  #   #then the regions should harmonize correctly in the formulas below
+  #   
+  #   nb <- poly2nb(geojson_red, queen = TRUE) #adjacent polygons are neighbors
+  #   adjacency <- nb2mat(nb, style = "B", zero.policy = TRUE) #converts it to an adjacency matrix which is passed to the bym2
+  #   
+  #   lagged_formula <- Cases ~ 1 + f(ID_spat_num, model = "bym2", graph = adjacency) +
+  #     f(ID_time_cyclic, model='rw1', cyclic=T, scale.model=T) + 
+  #     basis_meantemperature + basis_rainsum
+  # }
 
   model <- inla(formula = lagged_formula, data = df, family = "nbinomial", offset = log(E),
                 control.inla = list(strategy = 'adaptive'),
@@ -65,7 +82,6 @@ predict_chap <- function(model_fn, hist_fn, future_fn, preds_fn){
                 control.predictor = list(link = 1, compute = TRUE),
                 verbose = F, safe=FALSE)
   
-  #summary(model)
   casestopred <- df$Cases # response variable
   
   # Predict only for the cases where the response variable is missing
@@ -101,6 +117,15 @@ if (length(args) >= 1) {
   hist_fn <- args[2]
   future_fn <- args[3]
   preds_fn <- args[4]
+  #graph_fn <- args[5]
   
-  predict_chap(model_fn, hist_fn, future_fn, preds_fn)
+  predict_chap(model_fn, hist_fn, future_fn, preds_fn)#, graph_fn)
 }
+
+#Testing
+
+# model_fn <- "example_data_monthly/model"
+# hist_fn <- "example_data_monthly/historic_data.csv"
+# future_fn <- "example_data_monthly/future_data.csv"
+# preds_fn <- "example_data_monthly/predictions.csv"
+
